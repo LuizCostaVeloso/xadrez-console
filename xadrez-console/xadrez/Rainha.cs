fcee6c62d2c4f6ae6b4d89e1f02c14badaf18244
Luiz C Veloso <luizcveloso@gmail.com>
2021-12-06 14:08:23 -0400

    Jogado especial em passant

--- xadrez-console/xadrez/Rainha.cs
+++ xadrez-console/xadrez/Rainha.cs
@@ -3,10 +3,10 @@
 namespace xadrez
 {
 
-    class Dama : Peca
+    class Rainha : Peca
     {
 
-        public Dama(Tabuleiro tab, Cor cor) : base(tab, cor)
+        public Rainha(Tabuleiro tab, Cor cor) : base(tab, cor)
         {
         }
 
